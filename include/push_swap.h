7205b003d26d818d0c6eeb916a3841d7d500a672
Yenowme <76904042+Yenowme@users.noreply.github.com>
2022-01-26 21:19:24 +0900

    feat: parse arg

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jeong-yena <jeong-yena@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:26:27 by jeong-yena        #+#    #+#             */
-/*   Updated: 2022/01/26 16:28:05 by jeong-yena       ###   ########.fr       */
+/*   Updated: 2022/01/26 21:18:37 by jeong-yena       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,28 @@
 # include <stdio.h>
 
 # include "../libft/libft.h"
+
+# define FALSE 0
+# define TRUE 1
+typedef struct s_stack_node
+{
+	int					data;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
+
+typedef struct s_stack
+{
+	int					cnt;
+	t_stack_node		*head;
+	t_stack_node		*tail;
+}	t_stack;
+
+typedef struct s_ps
+{
+	t_stack	a;
+	t_stack	b;
+}	t_ps;
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,70 @@
 /*   By: jeong-yena <jeong-yena@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:25:14 by jeong-yena        #+#    #+#             */
-/*   Updated: 2022/01/26 16:33:22 by jeong-yena       ###   ########.fr       */
+/*   Updated: 2022/01/26 21:18:43 by jeong-yena       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-int	main(void)
+
+void	err_exit(char *str)
 {
-	printf("hello push_swap\n");
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	exit(1);
+}
+
+void	stack_add_back(char *str)
+{
+	//TODO: add stack a tail
+	printf("str: %s\n", str);
+}
+
+void	check_arg(char *str)
+{
+	char	**split;
+	int		is_split;
+	int		i;
+
+	i = 0;
+	is_split = FALSE;
+	while (ft_isdigit(str[i]) || str[i] == ' ')
+	{
+		if (str[i] == ' ')
+			is_split = TRUE;
+		i++;
+	}
+	if (str[i] != '\0')
+		err_exit("is not number\n");
+	else if (is_split)
+	{
+		split = ft_split(str, ' ');
+		while (*split)
+			stack_add_back(*split++);
+	}
+	else
+		stack_add_back(str);
+}
+
+void	parse_arg(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < argc - 1)
+		check_arg(argv[i]);
+}
+
+void	init_stack(t_ps *stack)
+{
+	ft_bzero(stack, sizeof(t_ps));
+}
+
+int	main(int argc, char **argv)
+{
+	t_ps	stack;
+
+	init_stack(&stack);
+	parse_arg(argc, argv);
 }
